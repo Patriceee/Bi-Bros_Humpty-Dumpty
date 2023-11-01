04730984c45497a349f00fbc08e38b19de702e28
Patriceee <114033857+Patriceee@users.noreply.github.com>
2023-11-01 16:29:03 -0500

    Update ArduinoBase.ino

--- ArduinoBase.ino
+++ ArduinoBase.ino
@@ -1,11 +1,9 @@
 void setup()
 {
-  // put your setup code here, to run once:
-
+  // patrice branch yippeeee
 }
 
 void loop() 
 {
-  // put your main code here, to run repeatedly:
-
+ //01010000 01000001 01010100 01010010 01001001 01000011 01000101 
 }
