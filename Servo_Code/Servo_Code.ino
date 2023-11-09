af03a609a51d7d4c3aa0c6f1a35d804e317a091e
eaudreyb <149627377+eaudreyb@users.noreply.github.com>
2023-11-08 18:47:38 -0600

    Update Servo_Code.ino
    Updated servo code. Servos now mode individually in correspondence with which piece of copper tape the player is put over.

--- Servo_Code/Servo_Code.ino
+++ Servo_Code/Servo_Code.ino
@@ -2,23 +2,25 @@
 
 Servo servo1;
 Servo servo2;
-const int buttonPin = 2;
+const int buttonPin1 = 2;
+const int buttonPin2 = 7;
 
 
 void setup() {
   servo1.attach(4);
   servo2.attach(5);
-  pinMode(buttonPin, INPUT);
+  pinMode(buttonPin1, INPUT);
+  pinMode(buttonPin2, INPUT);
 }
 
 void loop() {
-  if (digitalRead(buttonPin) == HIGH) {  //Activates the first servo to make Humpty Dumpty climb the wall
+  if (digitalRead(buttonPin1) == HIGH) {  //Activates the first servo to make Humpty Dumpty climb the wall
     servo1.write(90);
   } else {
     servo1.write(0);
   }
 
-  if (digitalRead(buttonPin) == HIGH) {  //Activates the second servo to flip the wall 180 degrees
+  if (digitalRead(buttonPin2) == HIGH) {  //Activates the second servo to flip the wall 180 degrees
     servo2.write(180);
   } else {
     servo2.write(0);
